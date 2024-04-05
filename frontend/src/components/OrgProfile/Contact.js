be4e6b87783fe3155ac005fd6e5e44835729c6c7
NSel1727 <noahseligson18@gmail.com>
2024-04-05 13:39:20 -0400

    Fix sunday hours

--- frontend/src/components/OrgProfile/Contact.js
+++ frontend/src/components/OrgProfile/Contact.js
@@ -235,7 +235,7 @@ function Contact(props) {
 										<Grid container justifyContent="right" alignItems="center" layout={'row'} className='times'>
 											<p className='dayText'>Sunday:</p>
 											{TimeSelector({label: "Start", value: sundayStart, onChange:(e) => {setSundayStart(e); if(!props.editInfo.current.hours.sunday) props.editInfo.current.hours.sunday = {}; props.editInfo.current.hours.sunday.start = e;}})}
-											{TimeSelector({label: "End", value: sundayEnd, onChange:(e) => {setSundayEnd(e); if(!props.editInfo.current.hours.sunday) props.editInfo.current.hours.sunday = {}; props.editInfo.current.hours.sunday.start = e;}})}
+											{TimeSelector({label: "End", value: sundayEnd, onChange:(e) => {setSundayEnd(e); if(!props.editInfo.current.hours.sunday) props.editInfo.current.hours.sunday = {}; props.editInfo.current.hours.sunday.end = e;}})}
 										</Grid>
 										<Grid container justifyContent="right" alignItems="center" layout={'row'} className='times'>
 											<p className='dayText'>Monday:</p>
